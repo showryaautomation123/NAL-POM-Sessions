dfe32ba2da9734db72d7b8d897bbaa5458f4f07a
showryaselenium <you@example.com>
2022-06-18 14:49:52 -0500

    added f3 feature

--- src/main/java/com/qa/opencart/pages/FeaturePage.java
+++ src/main/java/com/qa/opencart/pages/FeaturePage.java
@@ -5,6 +5,7 @@ public class FeaturePage {
 	public static void main(String[] args) {
 		System.out.println("this is feature f1");
 		System.out.println("this is feature f2");
+		System.out.println("this is feature f3");
 	}
 
 }
